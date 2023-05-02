885e70b4566b8fdc18d93cb82c4f3c1510ef719a
omartins-zs <gabrielmatheus209@gmail.com>
2023-05-02 10:16:00 -0300

    Fazendo Foreach da Language selecionada por Padrão

--- script.js
+++ script.js
@@ -4,9 +4,24 @@ const btnTranslate = document.querySelector("#btnTranslate");
 const selects = document.querySelectorAll("select");
 
 const countries = {
-  "en-GB": "Inglês",
-  "es-ES": "Espanhol",
-  "it-IT": "Italiano",
-  "ja-JP": "Japonês",
-  "pt-BR": "Português",
+    "en-GB": "Inglês",
+    "es-ES": "Espanhol",
+    "it-IT": "Italiano",
+    "ja-JP": "Japonês",
+    "pt-BR": "Português",
 };
+
+selects.forEach((tag) => {
+    for (let country in countries) {
+        let selected;
+        if (tag.className.includes("selectFrom") && country == "pt-BR") {
+            selected = "selected";
+        } else if (tag.className.includes("selectTo") && country == "en-GB") {
+            selected = "selected";
+        }
+
+        const option = `<option value="${country}" ${selected}>${countries[country]}</option>`;
+
+        tag.insertAdjacentHTML("beforeend", option);
+    }
+});
