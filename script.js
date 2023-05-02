bfe4a27c45e7815a81c6de3369ecbb5a3485fada
omartins-zs <gabrielmatheus209@gmail.com>
2023-05-02 10:14:18 -0300

    Declarando constantes do HTML e Languages

---
+++ script.js
@@ -0,0 +1,12 @@
+const textareaFrom = document.querySelector("#textareaFrom");
+const textareaTo = document.querySelector("#textareaTo");
+const btnTranslate = document.querySelector("#btnTranslate");
+const selects = document.querySelectorAll("select");
+
+const countries = {
+  "en-GB": "Inglês",
+  "es-ES": "Espanhol",
+  "it-IT": "Italiano",
+  "ja-JP": "Japonês",
+  "pt-BR": "Português",
+};
